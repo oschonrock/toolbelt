6341c2884e02a1b5015f08f81e26901ce4e22cd0
Oliver Schönrock <oliver@openbrackets.net>
2020-01-21 10:08:49 +0000

    refactor

--- os/bch.hpp
+++ os/bch.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <utility>
@@ -8,21 +9,35 @@
 namespace os::bch {
 // profiling
 
+using clk        = std::chrono::high_resolution_clock;
+using time_point = std::chrono::time_point<clk>;
+using std::chrono::duration;
+using std::chrono::duration_cast;
+
 class Timer {
 public:
-  explicit Timer(std::string label_) : start{std::chrono::high_resolution_clock::now()}, label{std::move(label_)} {}
+  explicit Timer(std::string label_) : start_{clk::now()}, label{std::move(label_)} {}
+
+  Timer(const Timer& other) = default;
+  Timer& operator=(const Timer& other) = default;
+  Timer(Timer&& other)                 = default;
+  Timer& operator=(Timer&& other) = default;
 
   ~Timer() { print(); }
 
   void print() {
-    finish = std::chrono::high_resolution_clock::now();
-    auto elapsed_ms =
-        std::chrono::duration_cast<std::chrono::duration<double>>(finish - start).count() * 1000;
-    std::cout << label << "=" << elapsed_ms << "ms\n";
+    finish_         = clk::now();
+    auto elapsed    = finish_ - start_;
+    auto elapsed_s  = duration_cast<duration<double>>(elapsed).count();
+    auto elapsed_ms = elapsed_s * 1000;
+    char buf[50]{0};                                                // NOLINT
+    std::sprintf(buf, "%-20s %10.4f ms", label.data(), elapsed_ms); // NOLINT
+    std::cerr << buf << '\n';                                       // NOLINT
   }
 
 private:
-  std::chrono::time_point<std::chrono::high_resolution_clock> start, finish;
+  time_point  start_;
+  time_point  finish_;
   std::string label;
 };
 
