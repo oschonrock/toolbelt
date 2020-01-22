5cf250a66e604720d4e31d67d2887a5dc2c38581
Oliver Schönrock <oliver@openbrackets.net>
2020-01-22 22:33:29 +0000

    hexdump

--- os/debug.hpp
+++ os/debug.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <algorithm>
-#include <array>
 #include <iomanip>
 #include <iostream>
 #include <list>
@@ -20,89 +18,6 @@ std::ostream& operator<<(std::ostream& stream, const std::pair<T, U>& pair) {
 namespace os {
 
 // some debug swiss army knives
-
-#ifndef DEBUG
-#define DEBUG 1 // not working conveniently from cmd line yet
-#endif
-
-#define DB(x)                                                                                      \
-  do {                                                                                             \
-    if (DEBUG) os::debug::db_impl(__FILE__, __LINE__, #x, x);                                      \
-  } while (0)
-
-template <typename Arg>
-void db_impl(const char* file, int line, const char* varname, Arg value) {
-  std::cout << file << ":" << line << ": warning: ";
-  std::cout << varname << " = " << value << '\n';
-}
-
-#define DBP(...)                                                                                   \
-  do {                                                                                             \
-    if (DEBUG) os::dbp_impl(__FILE__, __LINE__, __VA_ARGS__);                               \
-  } while (0)
-
-template <typename... Args>
-void dbp_impl(const char* file, int line, Args&&... args) {
-  std::cout << file << ":" << line << ": warning: ";
-  (std::cout << ... << std::forward<Args>(args)) << '\n';
-}
-
-inline std::ostream& hex_dump(std::ostream& os, const void* buffer, std::size_t bufsize,
-                       bool show_printable_chars = true) {
-  if (buffer == nullptr) {
-    return os;
-  }
-  constexpr std::size_t maxline{8};
-
-  auto old_format    = os.flags();
-  auto old_fill_char = os.fill();
-
-  // buffer for printable characters
-  unsigned char  pbuf[maxline + 1]; // NOLINT
-  unsigned char* pbuf_curr{pbuf};   // NOLINT
-
-  const unsigned char* buf{reinterpret_cast<const unsigned char*>(buffer)}; // NOLINT
-
-  for (std::size_t linecount = maxline; bufsize; --bufsize, ++buf) { // NOLINT
-    os << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned>(*buf) << ' ';
-    *pbuf_curr++ = std::isprint(*buf) ? *buf : '.'; // NOLINT
-    if (--linecount == 0) {
-      *pbuf_curr++ = '\0'; // NOLINT
-      if (show_printable_chars) {
-        os << " | " << pbuf; // NOLINT
-      }
-      os << '\n';
-      pbuf_curr = pbuf; // NOLINT
-      linecount = std::min(maxline, bufsize);
-    }
-  }
-  // finish incomplete line
-  if (pbuf_curr != pbuf) { // NOLINT
-    if (show_printable_chars) {
-      for (*pbuf_curr++ = '\0'; pbuf_curr != &pbuf[maxline + 1]; ++pbuf_curr) os << "   "; // NOLINT
-      os << " | " << pbuf;                                                                 // NOLINT
-    }
-    os << '\n';
-  }
-
-  os.fill(old_fill_char);
-  os.flags(old_format);
-  return os;
-}
-
-struct hd {
-  const void* buffer;
-  std::size_t bufsize;
-
-  hd(const void* buf, std::size_t bufsz) : buffer{buf}, bufsize{bufsz} {}
-
-  friend std::ostream& operator<<(std::ostream& out, const hd& hd) {
-    return hex_dump(out, hd.buffer, hd.bufsize, true);
-  }
-};
-
-} // namespace os
-
 // example usage
 
 // void f(int num, std::string msg) {
@@ -127,6 +42,136 @@ struct hd {
 //   f(3, "hello");
 //   return 0;
 // }
+
+#ifndef DEBUG
+#define DEBUG 1 // not working conveniently from cmd line yet
+#endif
+
+#define DB(x)                                                                                      \
+  do {                                                                                             \
+    if (DEBUG) os::db_impl(__FILE__, __LINE__, #x, x);                                             \
+  } while (0)
+
+template <typename Arg>
+void db_impl(const char* file, int line, const char* varname, Arg value) {
+  std::cout << file << ":" << line << ": warning: ";
+  std::cout << varname << " = " << value << '\n';
+}
+
+#define DBP(...)                                                                                   \
+  do {                                                                                             \
+    if (DEBUG) os::dbp_impl(__FILE__, __LINE__, __VA_ARGS__);                                      \
+  } while (0)
+
+template <typename... Args>
+void dbp_impl(const char* file, int line, Args&&... args) {
+  std::cout << file << ":" << line << ": warning: ";
+  (std::cout << ... << std::forward<Args>(args)) << '\n';
+}
+
+// utility: keeps states of an ostream, restores on destruction
+template <typename T>
+struct ostream_state {
+  explicit ostream_state(std::basic_ostream<T>& stream)
+      : stream_{stream}, flags_{stream.flags()}, fill_{stream.fill()}, width_{stream.width()} {}
+
+  ostream_state(const ostream_state& other) = delete;
+  ostream_state& operator=(const ostream_state& other) = delete;
+
+  ostream_state(ostream_state&& other) = delete;
+  ostream_state& operator=(ostream_state&& other) = delete;
+
+  ~ostream_state() {
+    stream_.flags(flags_);
+    stream_.fill(fill_);
+    stream_.width(width_);
+  }
+
+private:
+  std::basic_ostream<T>&                    stream_;
+  std::ios_base::fmtflags                   flags_;
+  typename std::basic_ostream<T>::char_type fill_;
+  std::streamsize                           width_;
+};
+
+inline std::ostream& print_adr(std::ostream& os, const void* adr) {
+  auto state = ostream_state(os);
+  return os << std::setw(19) << std::setfill(' ') << adr; // NOLINT
+}
+
+inline std::ostream& print_byte(std::ostream& os, const unsigned char* ptr) {
+  auto state = ostream_state(os);
+  return os << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned>(*ptr) << ' ';
+}
+
+inline std::ostream& print_ascii(std::ostream& os, const unsigned char* ptr) {
+  auto state = ostream_state(os);
+  return os << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned>(*ptr) << ' ';
+}
+
+inline std::ostream& hex_dump(std::ostream& os, const void* buffer, std::size_t bufsize) {
+  if (buffer == nullptr || bufsize == 0) return os;
+
+  constexpr std::size_t maxline{16};
+
+  // buffer for printable characters
+  unsigned char  pbuf[maxline + 1]; // NOLINT
+  unsigned char* pbuf_curr{pbuf};   // NOLINT
+
+  const unsigned char* buf{reinterpret_cast<const unsigned char*>(buffer)}; // NOLINT
+
+  // pre-buffer area: floor(nearest maxline)
+  size_t      offset    = reinterpret_cast<size_t>(buffer) % maxline; // NOLINT
+  std::size_t linecount = maxline;
+  if (offset > 0) {
+    const void* prebuf = buf - offset; // NOLINT
+    print_adr(os, prebuf) << ": ";
+    while (offset--) { // underflow OK NOLINT
+      os << "-- ";
+      *pbuf_curr++ = '.'; // NOLINT
+      --linecount;
+    }
+  }
+
+  // main buffer area
+  while (bufsize) {                                    // NOLINT
+    if (pbuf_curr == pbuf) print_adr(os, buf) << ": "; // NOLINT
+    print_byte(os, buf);
+    *pbuf_curr++ = std::isprint(*buf) ? *buf : '.'; // NOLINT
+    if (--linecount == 0) {
+      *pbuf_curr++ = '\0';         // NOLINT
+      os << " | " << pbuf << '\n'; // NOLINT
+      pbuf_curr = pbuf;            // NOLINT
+      linecount = std::min(maxline, bufsize);
+    }
+    --bufsize;
+    ++buf; // NOLINT
+  }
+
+  // post buffer area: finish incomplete line
+  if (pbuf_curr != pbuf) {                                                               // NOLINT
+    for (*pbuf_curr++ = '\0'; pbuf_curr != &pbuf[maxline + 1]; ++pbuf_curr) os << "-- "; // NOLINT
+    os << " | " << pbuf << '\n';                                                         // NOLINT
+  }
+  return os;
+}
+
+struct hd {
+  const void* buffer;
+  std::size_t bufsize;
+
+  hd(const void* buf, std::size_t bufsz) : buffer{buf}, bufsize{bufsz} {}
+
+  // doesn't really work. can't detect the appropriate size
+  // template <typename T>
+  // explicit hd(const T& buf, std::size_t bufsz = sizeof(T)) : buffer{buf}, bufsize{bufsz} {}
+
+  friend std::ostream& operator<<(std::ostream& out, const hd& hd) {
+    return hex_dump(out, hd.buffer, hd.bufsize);
+  }
+};
+
+} // namespace os
 
 // debug printing of containers
 
