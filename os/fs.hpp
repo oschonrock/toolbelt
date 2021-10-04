8aae743eb4b237c197d6b3482d59775005c99ca6
Oliver Schönrock <oliver@openbrackets.net>
2021-10-04 18:34:14 +0100

    access to FILE* from std::ifstream

--- os/fs.hpp
+++ os/fs.hpp
@@ -5,8 +5,24 @@
 #include <string_view>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <fstream>
+#include <ext/stdio_filebuf.h>
 
 namespace os::fs {
+
+// ontaining a FILE* from a std::ifstream or a std::ofstream
+// credit https://stackoverflow.com/a/19749019/1087626
+namespace impl {
+using buffer_t    = std::basic_ofstream<char>::__filebuf_type;
+using io_buffer_t = __gnu_cxx::stdio_filebuf<char>;
+inline FILE* cfile(buffer_t* const fb) {
+  return (static_cast<io_buffer_t* const>(fb))  // NOLINT this is NOT a dynamic cast
+      ->file(); // type std::__c_file
+}
+} // namespace impl
+
+inline FILE* cfile(std::ofstream const& ofs) { return impl::cfile(ofs.rdbuf()); }
+inline FILE* cfile(std::ifstream const& ifs) { return impl::cfile(ifs.rdbuf()); }
 
 class MemoryMappedFile {
 public:
@@ -17,15 +33,15 @@ public:
     // obtain file size
     struct stat sbuf {};
     if (fstat(fd, &sbuf) == -1) throw std::logic_error("MemoryMappedFile: cannot stat file size");
-    _filesize = sbuf.st_size;
+    filesize_ = static_cast<std::size_t>(sbuf.st_size);
 
-    _map = static_cast<const char*>(mmap(nullptr, _filesize, PROT_READ, MAP_PRIVATE, fd, 0U));
-    if (_map == MAP_FAILED) // NOLINT : doesn't work somehow?
+    map_ = static_cast<const char*>(mmap(nullptr, filesize_, PROT_READ, MAP_PRIVATE, fd, 0U));
+    if (map_ == MAP_FAILED) // NOLINT c-style cast in macro + int to ptr cast pessimisation
       throw std::logic_error("MemoryMappedFile: cannot map file");
   }
 
   ~MemoryMappedFile() {
-    if (munmap(static_cast<void*>(const_cast<char*>(_map)), _filesize) == -1) // NOLINT
+    if (munmap(static_cast<void*>(const_cast<char*>(map_)), filesize_) == -1) // NOLINT const_cast
       std::cerr << "Warnng: MemoryMappedFile: error in destructor during `munmap()`\n";
   }
 
@@ -38,16 +54,16 @@ public:
   MemoryMappedFile& operator=(MemoryMappedFile&& other) = default;
 
   // char* pointers. up to callee to make string_views or strings
-  [[nodiscard]] const char* begin() const { return _map; }
-  [[nodiscard]] const char* end() const { return _map + _filesize; } // NOLINT
+  [[nodiscard]] const char* begin() const { return map_; }
+  [[nodiscard]] const char* end() const { return map_ + filesize_; } // NOLINT
 
   [[nodiscard]] std::string_view get_buffer() const {
-    return std::string_view{begin(), static_cast<std::size_t>(end() - begin())} ;
+    return std::string_view{begin(), static_cast<std::size_t>(end() - begin())};
   }
 
 private:
-  size_t      _filesize = 0;
-  const char* _map      = nullptr;
+  std::size_t filesize_ = 0;
+  const char* map_      = nullptr;
 };
 
 } // namespace os::fs
